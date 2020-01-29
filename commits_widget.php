39887c668041810900f537383e3207a2efdab610
maxime fernandes <maximefernandes42@gmail.com>
2020-01-29 15:37:01 +0100

    Ajout de la méthode "getCommits" dans le widget

--- commits_widget.php
+++ commits_widget.php
@@ -8,6 +8,38 @@ class Commits_Widget extends WP_Widget
     {
         parent::__construct('commits', 'Commits', array('description' => 'Une liste des derniers commits d\'un dépôt.'));
     }
+
+    public function getCommits($owner, $repo, $nbr)
+    {
+        $client = new GitHubClient();
+        $client->setPage();
+        $client->setPageSize($nbr);
+        $commits = $client->repos->commits->listCommitsOnRepository($owner, $repo);
+
+        foreach($commits as $commit)
+        {
+            $committer = $commit->getCommitter()->getLogin();
+            $message = $commit->getCommit()->getMessage();
+            $sha = substr($commit->getSha(), 0, 7);
+            $url = $commit->getUrl();
+
+            ?>
+            <a href="<?php echo $url ?>">
+                <p>
+                    <?php
+                    echo " - Committer: " . $committer;
+                    ?>
+                </p>
+                <p>
+                    <?php
+                    echo $message . " - Sha: " . $sha . "... \n";
+                    ?>
+                <p>
+                <br/>
+            </a>
+            <?php
+        }
+    }
 }
 
 ?>
